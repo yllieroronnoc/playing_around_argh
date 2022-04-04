488ad69b5504fd7f9c3a71c3c7517636130ea2c9
ThomasJPSAtom <thomasjpshepard@gmail.com>
2022-04-04 12:22:56 -0600

    sense_node.py V1.0

--- src/men_at_work/sense_node.py
+++ src/men_at_work/sense_node.py
@@ -1,68 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
-#from math import fabs
-#from dynamixel_msgs.msg import JointState
-#from std_msgs.msg import Float64
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Point
 
-from std_msgs.msg import bool
-# should be more here?
+pub_sense = rospy.Publisher('sensing_node_input',Bool) # reset node
+pub_move = rospy.Publisher('sensing_node_boolean_move',Bool)
+pub_geom = rospy.Publisher('coordinate_tomato',Point)
 
-# THEIRS
-#goal_pos = 0;
-#pub = rospy.Publisher('/tilt_controller/command', Float64)
-
-# OURS
-#pub_sense = rospy.Publisher('/sensing_node_input',bool) # reset node
-#pub_move = ropsy.Publisher('no idea so help me god',probably a bool)
-#pub_geom = rospy.Publisher('im lost',geometery somehow)
-
-# THE TOPIC FOR SENSE NODE SHOULD BE sensing_node_input ??
-
-# SCRATCH THIS, DEFINE SIMILAR
-#def transform_callback(data):
-#    global goal_pos
-#    rospy.loginfo(rospy.get_name() + ': Current motor angle {0}'.format(data.current_pos))
-#
-#    # If the motor has reached its limit, publish a new command.
-#    if fabs(goal_pos-data.current_pos) < 0.01:
-#        if goal_pos == 0:
-#            goal_pos = 3.141592
-#        else:
-#            goal_pos = 0
-#
-#        str = "Time: {0} Moving motor to {1}" .format(rospy.get_time(), goal_pos)
-#        rospy.loginfo(str)
-#        pub.publish(Float64(goal_pos))
-
-# OUR OWN VERSION
 def callback(data):
-    rospy.loginfo()
-    # completely forgot what I was doing here but yeah
+    if(data.data==True):
+        #run script
 
-# SCRATCH THIS BUILD OWN
-#def dxl_control():
-#    rospy.init_node('dxl_control', anonymous=True)
-#    rospy.Subscriber('/tilt_controller/state', JointState, transform_callback).
-#    # Initial movement.
-#    pub.publish(Float64(goal_pos))
-#    rospy.spin()
+        #dummy values
+        tomato_found = True
+        geometry = Point()
+        geometry.position.x = 0
+        geometry.position.y = 0
+        geometry.position.z = 0
+
+        if(tomato_found):
+            pub_geom.publish(geometry)
+        else:
+            pub_move.publish(Bool(True))
+        pub_sense.publish(Bool(False))
 
 def sense_node():
-    ropsy.init_node('sense_node',anonymous=True)
-    #ropsy.Subscriber('/sensing_node_input',bool,callback)
-
-    #pub_sense.publish(False)
+    rospy.init_node('sense_node',anonymous=True)
+    rospy.Subscriber('sensing_node_input',Bool,callback)
     rospy.spin()
 
-# THINGS I DON'T KNOW WHERE TO PLACE
-#pub_move.publish(False)
-#pub_move.publish(True)
-#pub_geom.publish(idk???)
-
-# FINALLY, DON'T UNDERSTAND HOW WE CHANGE THIS, BUT NEEDS SCRATCH AND REDO
-#if __name__ == '__main__':
-#    try:
-#        dxl_control()
-#    except rospy.ROSInterruptException:
-#        pass
+if __name__ == '__main__':
+    try:
+        sense_node()
+    except rospy.ROSInterruptException:
+        pass
