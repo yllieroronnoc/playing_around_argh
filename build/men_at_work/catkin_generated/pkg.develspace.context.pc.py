16a399dcadbb32de3f56cfe5d4a8fa88f2b48325
Connor O'Reilly <coor1752@colorado.edu>
2022-04-03 22:32:19 -0600

    titied up move and control node, gripper control node somehow got deleted, need to fix that shit

--- build/men_at_work/catkin_generated/pkg.develspace.context.pc.py
+++ build/men_at_work/catkin_generated/pkg.develspace.context.pc.py
@@ -3,6 +3,6 @@ CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
 PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "men_at_work"
+PROJECT_NAME = "argh_nodes"
 PROJECT_SPACE_DIR = "/home/yllieroronnoc/playing_around_argh/devel"
 PROJECT_VERSION = "0.0.0"
